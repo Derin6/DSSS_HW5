91f9d2966c4c8264a0021ca404e0eb2bc941ae77
Derin6 <93079836+Derin6@users.noreply.github.com>
2022-12-03 14:14:47 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup (
+    name="snowflake",
+    version="0.1",
+    author="DSSS",
+    author_email="derin.cakiroglu@fau.de",
+    packages=find_packages(),
+    install_requires=["numpy","turtles"]
+
+
+
+)
+
+
+# In[ ]:
+
+
+
+
---
+++ snowflake/__main__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from . import let_it_snow
+
+let_it_snow.main()
+
